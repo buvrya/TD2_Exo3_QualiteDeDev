b833c570edd8f1f0a42f140bae3312a06ce6dcac
Alexy Buvry <alexy.buvry@etu.umontpellier.fr>
2024-09-12 09:48:42 +0200

    docs

--- src/main/java/fr/iut/editeur/commande/CommandeAjouter.java
+++ src/main/java/fr/iut/editeur/commande/CommandeAjouter.java
@@ -2,13 +2,23 @@ package fr.iut.editeur.commande;
 
 import fr.iut.editeur.document.Document;
 
+/**
+ * Classe CommandeAjouter : Permet d'ajouter du texte dans la classe Document.
+ */
 public class CommandeAjouter extends CommandeDocument {
 
-
+    /**
+     * Constructeur de la casse CommandeAjouter.
+     * @param document -> Objet de la classe Document.
+     * @param parameters -> Chaîne de caractères qui sera ajoutée.
+     */
     public CommandeAjouter(Document document, String[] parameters) {
         super(document, parameters);
     }
 
+    /**
+     * Récupère et lit l'entrée standard puis exécute la méthode adéquate.
+     */
     @Override
     public void executer() {
         if(parameters.length < 2) {
